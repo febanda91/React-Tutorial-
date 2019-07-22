c1b384178bef5e846f3e9c319896abb8de819be6
febanda01 <febanda91@gmail.com>
2019-07-22 17:06:36 -0500

    tweet-file

---
+++ projectreact/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './App.css';
+import Tweet from './tweet'
+
+
+function App() {
+
+  
+
+  return (
+    <div className="app">
+     <Tweet name="Frankie"/>
+     <Tweet name="Ted"/>
+     <Tweet name="Bob"/>
+     <Tweet name="Steve"/>
+    </div>
+  );
+}
+
+export default App;
