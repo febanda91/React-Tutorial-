3ff67dce24cf2e578d7f75db6610fbd9aa637b5f
febanda01 <febanda91@gmail.com>
2019-07-22 17:41:00 -0500

    tutorial-done

--- projectreact/src/App.js
+++ projectreact/src/App.js
@@ -1,18 +1,37 @@
-import React from 'react';
+import React, {useState} from 'react'
 import './App.css';
 import Tweet from './tweet'
 
 
 function App() {
 
-  
+  // const [isRed, setRed] = useState(false)
+  // const [count, setCount] = useState(0)
+
+  // const [user, setUser] = useState({
+  //   name: 'Frankie',
+  //   age: 28,
+  //   posts: ['my first post', 'my second post']
+  // })
+
+  // const increment = () => {
+  //   setCount(count + 1)
+  //   setRed(!isRed)
+  // }
+
+  const [users, setUsers] = useState([
+    {name: 'Frankie', message:'Hello there'},
+    {name: 'Ted', message: 'I am john snow'},
+    {name:'Jon', message: 'More followers'}
+
+  ])
+
 
   return (
     <div className="app">
-     <Tweet name="Frankie"/>
-     <Tweet name="Ted"/>
-     <Tweet name="Bob"/>
-     <Tweet name="Steve"/>
+      {users.map(user => (
+        <Tweet name={user.name} message={user.message}/>
+      ))}
     </div>
   );
 }
